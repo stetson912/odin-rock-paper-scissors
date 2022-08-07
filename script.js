db2ec1bdf61cf6d0445769b868de1cf40755b93a
stetson912 <trenton.stetson@gmail.com>
2022-08-06 21:59:40 -0700

    Make ui for rock paper scissors game

--- script.js
+++ script.js
@@ -26,36 +26,73 @@ const playRound = (playerSelection,computerSelection) => {
             return "Hmm... I think you're trying to cheat!!! Choose a valid option please."
     }
 
-    return result
+    // return result
+    updateScores(result)
 }
 
-
-const game = () => {
-    let playerScore = 0
-    let computerScore = 0
-    for(let i = 1; i<=5; i++){
-        let playerChoice = prompt("Please enter rock, paper, or scissors").toLowerCase()
-        // let playerChoice = "scissors"
-        let computerChoice = getComputerChoice()
-        let result = playRound(playerChoice,computerChoice)
-        console.log(result)
-        if(result.includes("Win")){
-            playerScore++
-        }
-        if(result.includes("Lose")){
-            computerScore++
-        }
+const updateScores = (result) => {
+    const playerScore = document.querySelector('.playerScoreDisplay')
+    const computerScore = document.querySelector('.compScoreDisplay')
+    let currentPlayerScore = parseInt(playerScore.textContent)
+    let currentCompScore = parseInt(computerScore.textContent)
+    let resultH1 = document.querySelector(".result")
+    
+    // if(currentPlayerScore == 5 || currentCompScore == 5){
+    //     playerScore.innerText = 0
+    //     computerScore.innerText = 0
+    //     return
+    // }
+    if(result.includes("Win") && !(currentPlayerScore == 5 || currentCompScore == 5) ){
+        playerScore.innerText = currentPlayerScore + 1
+        resultH1.innerText = result
+        console.log()
+    }else if (result.includes("Lose") && !(currentPlayerScore == 5 || currentCompScore == 5)) {
+        computerScore.innerText =currentCompScore + 1
+        resultH1.innerText = result
+    } else if (result.includes("Draw") && !(currentPlayerScore == 5 || currentCompScore == 5)){
+        resultH1.innerText = result
+    }
+    else{
+        resultH1.innerText = (currentPlayerScore > currentCompScore)? 'You Won the Game! Enter a choice to start a new game.' : 'You Lost! Enter a choice to start a new game.'
+        playerScore.innerText =0
+        computerScore.innerText =0
     }
 
-    if(playerScore < computerScore){
-        return console.log(`You lost this game ${playerScore} to ${computerScore}.`)
-    }
-    if(playerScore > computerScore){
-        return console.log(`You Won this game ${playerScore} to ${computerScore}.`)
-    }
-
-    return console.log(`This game is a Draw: ${playerScore} to ${computerScore}`)
-
+    
 }
+// const game = () => {
+//     let playerScore = 0
+//     let computerScore = 0
+//     for(let i = 1; i<=5; i++){
+//         // let playerChoice = prompt("Please enter rock, paper, or scissors").toLowerCase()
+//         // let playerChoice = "scissors"
+//         let computerChoice = getComputerChoice()
+//         let result = playRound(playerChoice,computerChoice)
+//         console.log(result)
+//         if(result.includes("Win")){
+//             playerScore++
+//         }
+//         if(result.includes("Lose")){
+//             computerScore++
+//         }
+//     }
 
-game()
+//     if(playerScore < computerScore){
+//         return console.log(`You lost this game ${playerScore} to ${computerScore}.`)
+//     }
+//     if(playerScore > computerScore){
+//         return console.log(`You Won this game ${playerScore} to ${computerScore}.`)
+//     }
+
+//     return console.log(`This game is a Draw: ${playerScore} to ${computerScore}`)
+
+// }
+
+// game()
+
+const choices = document.querySelectorAll('button')
+choices.forEach(choice => {
+    choice.addEventListener('click',(e)=>{
+        playRound(e.target.innerText,getComputerChoice())}
+    );
+})
